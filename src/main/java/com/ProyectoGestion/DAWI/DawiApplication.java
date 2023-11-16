ae1dcc7bb7c8ae6b5eb93a59783b0d3737a3e62f
Hollow <aoctester2017@gmail.com>
2023-11-15 23:06:35 -0500

    ProyectoDAWI

---
+++ src/main/java/com/ProyectoGestion/DAWI/DawiApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoGestion.DAWI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoGestion/DAWI/DawiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoGestion.DAWI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
